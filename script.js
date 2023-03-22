2ba768ae8f01fa8be22d189a8233599f799f2f93
Stephen Adebambo <steveoncaffeine@gmail.com>
2023-03-23 00:17:45 +0100

    Fix input bugs and add keyboard support

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+let numKeys = document.querySelectorAll(".num");
+let cancel = document.querySelector(".cancel");
+let display = document.querySelector(".display");
+let ops = document.querySelectorAll(".op");
+let calc = document.querySelector(".equal");
+let del = document.querySelector(".delete");
+let currentOp = "";
+let valuesList = [];
+let opsList = [];
+let opChange = false;
+
 function add (num1, num2) {
     return num1 + num2;
 }
@@ -24,57 +35,13 @@ function operate(operator, num1, num2) {
     }
 }
 
-let numKeys = document.querySelectorAll(".num");
-let cancel = document.querySelector(".cancel");
-let display = document.querySelector(".display");
-let ops = document.querySelectorAll(".op");
-let calc = document.querySelector(".equal");
-let del = document.querySelector(".delete");
-let currentOp = "";
-let valuesList = [];
-let opsList = [];
-let opChange = false;
+function deleteText() {
+    if (display.textContent !== "") {
+        display.textContent = display.textContent.slice(0, display.textContent.length - 1);
+    }
+}
 
-numKeys.forEach(key => {
-    key.addEventListener("click", event => {
-        ops.forEach(op => op.style.backgroundColor = "rgb(9, 189, 150)");
-        if (isNaN(display.textContent) || display.textContent === "0") {
-            display.textContent = "";
-        }
-        if (opChange === true) {
-            opsList.push(currentOp);
-            opChange = false;
-        }
-        if (display.textContent.length <= 9) {
-            if (event.target.textContent.includes(".")) {
-                if (display.textContent === "") {
-                    display.textContent = "0.";
-                }
-                else if (!display.textContent.includes(".")) {
-                    display.textContent = display.textContent + ".";
-                }
-            }
-            else {
-                display.textContent = display.textContent + event.target.textContent;
-            }
-        }
-    });
-});
-
-ops.forEach(op => {
-    op.addEventListener("click", event => {
-        ops.forEach(op => op.style.backgroundColor = "rgb(9, 189, 150)");
-        if (display.textContent != "") valuesList.push(Number(display.textContent));
-        if (valuesList.length !== 0) {
-            display.textContent = "";
-            currentOp = event.target.textContent;
-            event.target.style.backgroundColor = "rgb(5, 110, 88)";
-            opChange = true;
-        }
-    });
-});
-
-calc.addEventListener("click", () => {
+function calculateValue() {
     ops.forEach(op => op.style.backgroundColor = "rgb(9, 189, 150)");
     if (display.textContent != "") valuesList.push(Number(display.textContent));
     let value = valuesList[0];
@@ -100,17 +67,86 @@ calc.addEventListener("click", () => {
     valuesList.length = 0;
     opsList.length = 0;
     currentOp = "";
-});
+}
 
-cancel.addEventListener("click", () => {
+function clearDisplay() {
     valuesList.length = 0;
     opsList.length = 0;
     currentOp = "";
     display.textContent = "";
+}
+
+function inputNum(event, source) {
+    ops.forEach(op => op.style.backgroundColor = "rgb(9, 189, 150)");
+    if (isNaN(display.textContent) || display.textContent === "0") {
+        display.textContent = "";
+    }
+    if (opChange === true) {
+        opsList.push(currentOp);
+        opChange = false;
+    }
+    if (display.textContent.length <= 9) {
+        if (source.includes(".")) {
+            if (display.textContent === "") {
+                display.textContent = "0.";
+            }
+            else if (!display.textContent.includes(".")) {
+                display.textContent = display.textContent + ".";
+            }
+        }
+        else {
+            display.textContent = display.textContent + source;
+        }
+    }
+}
+
+function performOp(event, source) {
+    ops.forEach(op => op.style.backgroundColor = "rgb(9, 189, 150)");
+    if (display.textContent != "") valuesList.push(Number(display.textContent));
+    if (valuesList.length !== 0) {
+        display.textContent = "";
+        currentOp = source;
+        ops.forEach(op => {
+            if (op.textContent === source) {
+                op.style.backgroundColor = "rgb(5, 110, 88)";
+            }
+        });
+        opChange = true;
+    }
+}
+
+numKeys.forEach(key => {
+    key.addEventListener("click", event => {inputNum(event, event.target.textContent)});
 });
 
-del.addEventListener("click", () => {
-    if (display.textContent !== "") {
-        display.textContent = display.textContent.slice(0, display.textContent.length - 1);
+ops.forEach(op => {
+    op.addEventListener("click", event => {performOp(event, event.target.textContent)});
+});
+
+calc.addEventListener("click", calculateValue);
+
+cancel.addEventListener("click", clearDisplay);
+
+del.addEventListener("click", deleteText);
+
+document.addEventListener("keydown", event => {
+    if (event.key === "Backspace") {
+        deleteText();
+    }
+
+    if (event.key === "=") {
+        calculateValue();
+    }
+    
+    if (!isNaN(event.key) || event.key === ".") {
+        inputNum(event, event.key);
+    }
+
+    if (event.key.toLowerCase() === "c") {
+        clearDisplay();
+    }
+
+    if ("+ - * /".includes(event.key)) {
+        performOp(event, event.key);
     }
 });
