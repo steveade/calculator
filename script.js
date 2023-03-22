2670f0812a55674b80083277cc87ba86e9e18b79
Stephen Adebambo <steveoncaffeine@gmail.com>
2023-03-22 02:56:10 +0100

    Add feature to display numbers clicked on the calculator

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function add (num1, num2) {
 }
 
 function subtract(num1, num2) {
-    return num1 + num2;
+    return num1 - num2;
 }
 
 function multiply(num1, num2) {
@@ -14,3 +14,37 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case "+": return add(num1, num2);
+        case "-": return subtract(num1, num2);
+        case "*": return multiply(num1, num2);
+        case "/": return divide(num1, num2);
+        default: return 'unknown op';
+    }
+}
+
+let numKeys = document.querySelectorAll(".num");
+let cancel = document.querySelector(".cancel");
+let display = document.querySelector(".display");
+let ops = document.querySelectorAll(".op");
+let calc = document.querySelector(".equal");
+let valuesList = [];
+
+numKeys.forEach(key => {
+    key.addEventListener("click", event => {
+        if (display.textContent.length <= 9) {
+            if (event.target.textContent.includes(".")) {
+                if (display.textContent === "") {
+                    display.textContent = "0.";
+                }
+                else if (!display.textContent.includes(".")) {
+                    display.textContent = display.textContent + ".";
+                }
+            }
+            else {
+                display.textContent = display.textContent + event.target.textContent;
+            }
+        }
+    });
+});
